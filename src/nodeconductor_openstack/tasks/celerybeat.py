9e5f1830453ddde99ade377cce53eb14a9d323eb
Pavel Marchuk <marchukpavelp@gmail.com>
2016-06-22 18:57:18 +0300

    Refactor pull operations
     - nc-1275
     - do not modify pulled object if was changed after information pull.
     - do not except all error on security groups and floating IPs pull.

--- src/nodeconductor_openstack/tasks/celerybeat.py
+++ src/nodeconductor_openstack/tasks/celerybeat.py
@@ -1,14 +1,18 @@
+import logging
+
 from celery import shared_task, chain
 
 from nodeconductor.core import tasks as core_tasks, utils as core_utils
+from nodeconductor.structure import ServiceBackendError
+
 from . import models
+
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task(name='nodeconductor.openstack.pull_tenants')
 def pull_tenants():
-    # XXX: It is unsafe to pull tenant without any checks.
-    #      This can lead to conflicts with user updates in case of
-    #      simultaneous operations. (Issue NC-1275)
     for tenant in models.Tenant.objects.filter(state=models.Tenant.States.ERRED):
         serialized_tenant = core_utils.serialize_instance(tenant)
         core_tasks.BackendMethodTask().apply_async(
@@ -35,10 +39,49 @@ def recover_tenant(serialized_tenant):
 def pull_tenants_properties():
     for tenant in models.Tenant.objects.filter(state=models.Tenant.States.OK):
         serialized_tenant = core_utils.serialize_instance(tenant)
-        # XXX: It is unsafe to pull security groups without any checks.
-        #      This can lead to conflicts with user updates in case of
-        #      simultaneous operations. (Issue NC-1275)
         core_tasks.BackendMethodTask().delay(serialized_tenant, 'pull_tenant_security_groups')
         core_tasks.BackendMethodTask().delay(serialized_tenant, 'pull_tenant_floating_ips')
         core_tasks.BackendMethodTask().delay(serialized_tenant, 'pull_tenant_quotas')
-        core_tasks.BackendMethodTask().delay(serialized_tenant, 'pull_tenant_instances')
+
+
+@shared_task(name='nodeconductor.openstack.pull_instances')
+def pull_instances():
+    for tenant in models.Tenant.objects.exclude(state=models.Tenant.States.ERRED):
+        # group instance update by tenant to reduce amount of logins and update
+        # all instance with one session.
+        # Ideally session should be cached in backend module.
+        pull_tenant_instances.delay(core_utils.serialize_instance(tenant))
+
+
+@shared_task
+def pull_tenant_instances(serialized_tenant):
+    tenant = core_utils.deserialize_instance(serialized_tenant)
+    backend = tenant.get_backend()
+    States = models.Instance.States
+    stable_states = [States.ONLINE, States.OFFLINE, States.ERRED]
+    for instance in tenant.instances.filter(state__in=stable_states).exclude(backend_id=''):
+        try:
+            backend.pull_instance(instance)
+        except ServiceBackendError:
+            logger.error('Failed to pull instance %s (PK: %s).' % (instance.name, instance.pk))
+
+
+@shared_task(name='nodeconductor.openstack.pull_volumes')
+def pull_volumes():
+    for tenant in models.Tenant.objects.exclude(state=models.Tenant.States.ERRED):
+        # group volumes update by tenant to reduce amount of logins and update
+        # all volumes with one session.
+        # Ideally session should be cached in backend module.
+        pull_tenant_volumes.delay(core_utils.serialize_instance(tenant))
+
+
+@shared_task
+def pull_tenant_volumes(serialized_tenant):
+    tenant = core_utils.deserialize_instance(serialized_tenant)
+    backend = tenant.get_backend()
+    States = models.Volume.States
+    for volume in tenant.volumes.filter(state__in=[States.OK, States.ERRED]).exclude(backend_id=''):
+        try:
+            backend.pull_volume(volume)
+        except ServiceBackendError:
+            logger.error('Failed to pull volume %s (PK: %s).' % (volume.name, volume.pk))
