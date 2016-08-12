e2823ea8a95e663af21f3699e7bb2d431abc57f9
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2016-08-12 11:52:01 +0300

    Fix tests
    - NC-1514

--- src/nodeconductor_openstack/tests/test_fields.py
+++ src/nodeconductor_openstack/tests/test_fields.py
@@ -6,41 +6,29 @@ from nodeconductor.core import utils as core_utils
 from nodeconductor_openstack.fields import StringTimestampField
 
 
-class StringTimestampSerializer(serializers.Serializer):
-    content = StringTimestampField(formats=('%Y-%m-%dT%H:%M:%S',))
-
-
 class StringTimestampFieldTest(unittest.TestCase):
     def setUp(self):
+        self.field = StringTimestampField(formats=('%Y-%m-%dT%H:%M:%S',))
         self.datetime = core_utils.timeshift()
         self.datetime_str = self.datetime.strftime('%Y-%m-%dT%H:%M:%S')
         self.timestamp = core_utils.datetime_to_timestamp(self.datetime)
 
     def test_datetime_string_serialized_as_timestamp(self):
-        serializer = StringTimestampSerializer(instance={'content': self.datetime_str})
-        actual = serializer.data['content']
+        actual = self.field.to_representation(self.datetime_str)
         self.assertEqual(self.timestamp, actual)
 
     def test_timestamp_parsed_as_datetime_string(self):
-        serializer = StringTimestampSerializer(data={'content': self.timestamp})
-        self.assertTrue(serializer.is_valid())
-        actual = serializer.validated_data['content']
+        actual = self.field.to_internal_value(self.timestamp)
         self.assertEqual(self.datetime_str, actual)
 
     def test_incorrect_timestamp(self):
-        serializer = StringTimestampSerializer(data={'content': 'NOT_A_UNIX_TIMESTAMP'})
-        self.assertFalse(serializer.is_valid())
-        self.assertIn('content', serializer.errors,
-                      'There should be errors for content field')
-        self.assertIn('Value "NOT_A_UNIX_TIMESTAMP" should be valid UNIX timestamp.',
-                      serializer.errors['content'])
+        with self.assertRaises(serializers.ValidationError):
+            self.field.to_internal_value("NOT_A_UNIX_TIMESTAMP")
 
     def test_incorrect_datetime_string(self):
         datetime_str = self.datetime.strftime('%Y-%m-%d')
-        serializer = StringTimestampSerializer(instance={'content': datetime_str})
-
         with self.assertRaises(serializers.ValidationError):
-            serializer.data['content']
+            self.field.to_representation(datetime_str)
 
     def test_field_initialization_without_formats(self):
         with self.assertRaises(AssertionError):
