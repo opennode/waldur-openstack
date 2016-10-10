cd60c252859b35286f0ebe2d98f3d86063c63dcc
Pavel Marchuk <marchukpavelp@gmail.com>
2016-10-10 17:18:27 +0300

    Fix volume extension validation again
     - wal-65

--- src/nodeconductor_openstack/serializers.py
+++ src/nodeconductor_openstack/serializers.py
@@ -956,7 +956,7 @@ class VolumeExtendSerializer(serializers.Serializer):
             raise serializers.ValidationError({
                 'non_field_errors': ['Unable to extend volume without backend_id']
             })
-        if volume.instance.state != models.Instance.States.OFFLINE:
+        if volume.instance and volume.instance.state != models.Instance.States.OFFLINE:
             raise serializers.ValidationError({
                 'non_field_errors': ['Volume instance should be in OFFLINE state']
             })
