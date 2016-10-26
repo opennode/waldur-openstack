3f31a03110151946f4902a331049467435cc40de
Victor Mireyev <victor@opennodecloud.com>
2016-10-26 17:49:26 +0300

    Fix service create for customer owner. Add unit tests.

--- src/nodeconductor_openstack/tests/test_tenant.py
+++ src/nodeconductor_openstack/tests/test_tenant.py
@@ -1,19 +1,19 @@
+from ddt import data, ddt
 from mock import patch
 
 from rest_framework import test, status
 from nodeconductor.structure.tests import factories as structure_factories
 from nodeconductor_openstack.models import Tenant, OpenStackService
 
-from . import factories
+from . import factories, fixtures
 
 
 class BaseTenantActionsTest(test.APISimpleTestCase):
 
     def setUp(self):
         super(BaseTenantActionsTest, self).setUp()
-        self.tenant = factories.TenantFactory()
-        staff = structure_factories.UserFactory(is_staff=True)
-        self.client.force_authenticate(user=staff)
+        self.fixture = fixtures.OpenStackFixture()
+        self.tenant = self.fixture.openstack_tenant
 
 
 @patch('nodeconductor_openstack.executors.TenantPushQuotasExecutor.execute')
@@ -26,6 +26,7 @@ class TenantQuotasTest(BaseTenantActionsTest):
         self.assertFalse(mocked_task.called)
 
     def test_staff_can_set_tenant_quotas(self, mocked_task):
+        self.client.force_authenticate(self.fixture.staff)
         quotas_data = {'security_group_count': 100, 'security_group_rule_count': 100}
         response = self.client.post(self.get_url(), data=quotas_data)
 
@@ -42,6 +43,7 @@ class TenantDeleteExternalNetworkTest(BaseTenantActionsTest):
         self.tenant.external_network_id = 'abcd1234'
         self.tenant.save()
 
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.delete(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         mocked_task.assert_called_once_with(self.tenant)
@@ -50,6 +52,7 @@ class TenantDeleteExternalNetworkTest(BaseTenantActionsTest):
         self.tenant.external_network_id = ''
         self.tenant.save()
 
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.delete(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(mocked_task.called)
@@ -69,6 +72,7 @@ class TenantCreateExternalNetworkTest(BaseTenantActionsTest):
             'ips_count': 6
         }
 
+        self.client.force_authenticate(self.fixture.staff)
         url = factories.TenantFactory.get_url(self.tenant, 'external_network')
         response = self.client.post(url, payload)
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
@@ -81,6 +85,7 @@ class TenantFloatingIPTest(BaseTenantActionsTest):
         self.tenant.external_network_id = ''
         self.tenant.save()
 
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.post(factories.TenantFactory.get_url(self.tenant, 'allocate_floating_ip'))
         self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
         self.assertEqual(response.data['detail'], 'Tenant should have an external network ID.')
@@ -90,6 +95,7 @@ class TenantFloatingIPTest(BaseTenantActionsTest):
         tenant = factories.TenantFactory(external_network_id='12345', state=Tenant.States.ERRED)
         url = factories.TenantFactory.get_url(tenant, 'allocate_floating_ip')
 
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.post(url)
         self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
         self.assertEqual(response.data['detail'], 'Tenant should be in state OK.')
@@ -99,6 +105,7 @@ class TenantFloatingIPTest(BaseTenantActionsTest):
         tenant = factories.TenantFactory(external_network_id='12345')
         url = factories.TenantFactory.get_url(tenant, 'allocate_floating_ip')
 
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.post(url)
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         self.assertEqual(response.data['detail'], 'Floating IP allocation has been scheduled.')
@@ -109,6 +116,7 @@ class TenantFloatingIPTest(BaseTenantActionsTest):
 @patch('nodeconductor_openstack.executors.TenantPullExecutor.execute')
 class TenantPullTest(BaseTenantActionsTest):
     def test_staff_can_pull_tenant(self, mocked_task):
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.post(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         mocked_task.assert_called_once_with(self.tenant)
@@ -118,6 +126,7 @@ class TenantPullTest(BaseTenantActionsTest):
         settings.options['is_admin'] = False
         settings.save()
 
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.post(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(mocked_task.called)
@@ -129,6 +138,7 @@ class TenantPullTest(BaseTenantActionsTest):
 @patch('nodeconductor_openstack.executors.TenantDeleteExecutor.execute')
 class TenantDeleteTest(BaseTenantActionsTest):
     def test_staff_can_delete_tenant(self, mocked_task):
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.delete(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         mocked_task.assert_called_once_with(self.tenant, async=True, force=False)
@@ -138,6 +148,7 @@ class TenantDeleteTest(BaseTenantActionsTest):
         settings.options['is_admin'] = False
         settings.save()
 
+        self.client.force_authenticate(self.fixture.staff)
         response = self.client.delete(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(mocked_task.called)
@@ -146,6 +157,7 @@ class TenantDeleteTest(BaseTenantActionsTest):
         return factories.TenantFactory.get_url(self.tenant)
 
 
+@ddt
 class ServiceTenantCreateTest(BaseTenantActionsTest):
 
     def setUp(self):
@@ -153,8 +165,10 @@ class ServiceTenantCreateTest(BaseTenantActionsTest):
         self.settings = self.tenant.service_project_link.service.settings
         self.url = factories.TenantFactory.get_url(self.tenant, 'create_service')
 
+    @data('owner', 'staff')
     @patch('nodeconductor.structure.executors.ServiceSettingsCreateExecutor.execute')
-    def test_can_create_tenant_and_non_admin_service(self, mocked_execute):
+    def test_can_create_service(self, user, mocked_execute):
+        self.client.force_authenticate(getattr(self.fixture, user))
         response = self.client.post(self.url, {'name': 'Valid service'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(mocked_execute.called)
@@ -166,3 +180,17 @@ class ServiceTenantCreateTest(BaseTenantActionsTest):
             settings__username=self.tenant.user_username,
             settings__password=self.tenant.user_password
         ).exists())
+
+    @data('manager', 'admin')
+    def test_can_not_create_service(self, user):
+        self.client.force_authenticate(getattr(self.fixture, user))
+        response = self.client.post(self.url, {'name': 'Valid service'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_can_not_create_service_from_erred_tenant(self):
+        self.tenant.state = Tenant.States.ERRED
+        self.tenant.save()
+
+        self.client.force_authenticate(self.fixture.owner)
+        response = self.client.post(self.url, {'name': 'Valid service'})
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
