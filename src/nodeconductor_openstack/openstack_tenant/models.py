30ff2ecab16ce1d35a9cdf8a9429c0607c3a1dd8
Pavel Marchuk <marchukpavelp@gmail.com>
2016-10-27 15:50:28 +0300

    Move SG rules common part to openstack_base
     - wal-93

--- src/nodeconductor_openstack/openstack_tenant/models.py
+++ src/nodeconductor_openstack/openstack_tenant/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+from nodeconductor.core import models as core_models
+from nodeconductor.logging.loggers import LoggableMixin
 from nodeconductor.structure import models as structure_models
+
+from nodeconductor_openstack.openstack_base import models as openstack_base_models
 
 
 class OpenStackTenantService(structure_models.Service):
@@ -18,7 +23,6 @@ class OpenStackTenantService(structure_models.Service):
 
 
 class OpenStackTenantServiceProjectLink(structure_models.ServiceProjectLink):
-
     service = models.ForeignKey(OpenStackTenantService)
 
     class Meta(structure_models.ServiceProjectLink.Meta):
@@ -28,3 +32,33 @@ class OpenStackTenantServiceProjectLink(structure_models.ServiceProjectLink):
     @classmethod
     def get_url_name(cls):
         return 'openstacktenant-spl'
+
+
+class Flavor(LoggableMixin, structure_models.ServiceProperty):
+    cores = models.PositiveSmallIntegerField(help_text='Number of cores in a VM')
+    ram = models.PositiveIntegerField(help_text='Memory size in MiB')
+    disk = models.PositiveIntegerField(help_text='Root disk size in MiB')
+
+
+class Image(structure_models.ServiceProperty):
+    min_disk = models.PositiveIntegerField(default=0, help_text='Minimum disk size in MiB')
+    min_ram = models.PositiveIntegerField(default=0, help_text='Minimum memory size in MiB')
+
+
+class SecurityGroup(core_models.DescribableMixin, structure_models.ServiceProperty):
+    pass
+
+
+class SecurityGroupRule(openstack_base_models.BaseSecurityGroupRule):
+    security_group = models.ForeignKey(SecurityGroup, related_name='rules')
+
+
+@python_2_unicode_compatible
+class FloatingIP(structure_models.ServiceProperty):
+    address = models.GenericIPAddressField(protocol='IPv4')
+    status = models.CharField(max_length=30)
+    backend_network_id = models.CharField(max_length=255, editable=False)
+    is_booked = models.BooleanField(default=False, help_text='Defines is FloatingIP booked by NodeConductor.')
+
+    def __str__(self):
+        return '%s:%s | %s' % (self.address, self.status, self.settings)
