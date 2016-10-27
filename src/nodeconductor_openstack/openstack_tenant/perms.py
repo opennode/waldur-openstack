01847c64869298a436980eb17c549d49f09d6a91
Pavel Marchuk <marchukpavelp@gmail.com>
2016-10-27 17:47:58 +0300

    Add permissions to service properties
     - wal-94

--- src/nodeconductor_openstack/openstack_tenant/perms.py
+++ src/nodeconductor_openstack/openstack_tenant/perms.py
@@ -1,7 +1,12 @@
+from nodeconductor.core.permissions import StaffPermissionLogic
 from nodeconductor.structure import perms as structure_perms
 
 
 PERMISSION_LOGICS = (
     ('openstack_tenant.OpenStackTenantService', structure_perms.service_permission_logic),
     ('openstack_tenant.OpenStackTenantServiceProjectLink', structure_perms.service_project_link_permission_logic),
+    ('openstack_tenant.Flavor', StaffPermissionLogic(any_permission=True)),
+    ('openstack_tenant.Image', StaffPermissionLogic(any_permission=True)),
+    ('openstack_tenant.FloatingIP', StaffPermissionLogic(any_permission=True)),
+    ('openstack_tenant.SecurityGroup', StaffPermissionLogic(any_permission=True)),
 )
