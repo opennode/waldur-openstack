4fd87ee24a47ee09f588c1bd1054ce763c181f18
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2016-12-27 12:48:32 +0200

    Drop handlers, perms for tenant resources
    - WAL-278

--- src/nodeconductor_openstack/openstack/handlers.py
+++ src/nodeconductor_openstack/openstack/handlers.py
@@ -75,26 +75,6 @@ def change_floating_ip_quota_on_status_change(sender, instance, created=False, *
         add_quota('floating_ip_count', -1)
 
 
-def log_backup_schedule_save(sender, instance, created=False, **kwargs):
-    if created:
-        event_logger.openstack_backup.info(
-            'Backup schedule for {resource_name} has been created.',
-            event_type='resource_backup_schedule_creation_succeeded',
-            event_context={'resource': instance.instance})
-    else:
-        event_logger.openstack_backup.info(
-            'Backup schedule for {resource_name} has been updated.',
-            event_type='resource_backup_schedule_update_succeeded',
-            event_context={'resource': instance.instance})
-
-
-def log_backup_schedule_delete(sender, instance, **kwargs):
-    event_logger.openstack_backup.info(
-        'Backup schedule for {resource_name} has been deleted.',
-        event_type='resource_backup_schedule_deletion_succeeded',
-        event_context={'resource': instance.instance})
-
-
 def remove_ssh_key_from_tenants(sender, structure, user, role, **kwargs):
     """ Delete user ssh keys from tenants that he does not have access now. """
     tenants = Tenant.objects.filter(**{sender.__name__.lower(): structure})
