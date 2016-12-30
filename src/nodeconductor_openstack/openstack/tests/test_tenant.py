b1f0f5c66151c088a49b95846bd5f1d165672edc
Pavel Marchuk <marchukpavelp@gmail.com>
2016-12-30 15:07:12 +0200

    Fix tests
     - wal-288

--- src/nodeconductor_openstack/openstack/tests/test_tenant.py
+++ src/nodeconductor_openstack/openstack/tests/test_tenant.py
@@ -22,7 +22,7 @@ class TenantQuotasTest(BaseTenantActionsTest):
         self.client.force_authenticate(user=structure_factories.UserFactory())
         response = self.client.post(self.get_url())
 
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertFalse(mocked_task.called)
 
     def test_staff_can_set_tenant_quotas(self, mocked_task):
@@ -37,82 +37,6 @@ class TenantQuotasTest(BaseTenantActionsTest):
         return factories.TenantFactory.get_url(self.tenant, 'set_quotas')
 
 
-@patch('nodeconductor_openstack.openstack.executors.TenantDeleteExternalNetworkExecutor.execute')
-class TenantDeleteExternalNetworkTest(BaseTenantActionsTest):
-    def test_staff_user_can_delete_existing_external_network(self, mocked_task):
-        self.tenant.external_network_id = 'abcd1234'
-        self.tenant.save()
-
-        self.client.force_authenticate(self.fixture.staff)
-        response = self.client.delete(self.get_url())
-        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
-        mocked_task.assert_called_once_with(self.tenant)
-
-    def test_staff_user_cannot_delete_not_existing_external_network(self, mocked_task):
-        self.tenant.external_network_id = ''
-        self.tenant.save()
-
-        self.client.force_authenticate(self.fixture.staff)
-        response = self.client.delete(self.get_url())
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertFalse(mocked_task.called)
-
-    def get_url(self):
-        return factories.TenantFactory.get_url(self.tenant, 'external_network')
-
-
-@patch('nodeconductor_openstack.openstack.executors.TenantCreateExternalNetworkExecutor.execute')
-class TenantCreateExternalNetworkTest(BaseTenantActionsTest):
-
-    def test_staff_user_can_create_external_network(self, mocked_task):
-        payload = {
-            'vlan_id': '2007',
-            'network_ip': '10.7.122.0',
-            'network_prefix': 26,
-            'ips_count': 6
-        }
-
-        self.client.force_authenticate(self.fixture.staff)
-        url = factories.TenantFactory.get_url(self.tenant, 'external_network')
-        response = self.client.post(url, payload)
-        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
-        mocked_task.assert_called_once_with(self.tenant, external_network_data=payload)
-
-
-@patch('nodeconductor_openstack.openstack.executors.TenantAllocateFloatingIPExecutor.execute')
-class TenantFloatingIPTest(BaseTenantActionsTest):
-    def test_staff_cannot_allocate_floating_ip_from_tenant_without_external_network_id(self, mocked_task):
-        self.tenant.external_network_id = ''
-        self.tenant.save()
-
-        self.client.force_authenticate(self.fixture.staff)
-        response = self.client.post(factories.TenantFactory.get_url(self.tenant, 'allocate_floating_ip'))
-        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
-        self.assertEqual(response.data['detail'], 'Tenant should have an external network ID.')
-        self.assertFalse(mocked_task.called)
-
-    def test_staff_cannot_allocate_floating_ip_from_tenant_in_unstable_state(self, mocked_task):
-        tenant = factories.TenantFactory(external_network_id='12345', state=Tenant.States.ERRED)
-        url = factories.TenantFactory.get_url(tenant, 'allocate_floating_ip')
-
-        self.client.force_authenticate(self.fixture.staff)
-        response = self.client.post(url)
-        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
-        self.assertEqual(response.data['detail'], 'Tenant should be in state OK.')
-        self.assertFalse(mocked_task.delay.called)
-
-    def test_staff_can_allocate_floating_ip_from_tenant_with_external_network_id(self, mocked_task):
-        tenant = factories.TenantFactory(external_network_id='12345')
-        url = factories.TenantFactory.get_url(tenant, 'allocate_floating_ip')
-
-        self.client.force_authenticate(self.fixture.staff)
-        response = self.client.post(url)
-        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
-        self.assertEqual(response.data['detail'], 'Floating IP allocation has been scheduled.')
-
-        mocked_task.assert_called_once_with(tenant)
-
-
 @patch('nodeconductor_openstack.openstack.executors.TenantPullExecutor.execute')
 class TenantPullTest(BaseTenantActionsTest):
     def test_staff_can_pull_tenant(self, mocked_task):
@@ -120,16 +44,6 @@ class TenantPullTest(BaseTenantActionsTest):
         response = self.client.post(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         mocked_task.assert_called_once_with(self.tenant)
-
-    def test_staff_can_not_pull_tenant_from_non_admin_service(self, mocked_task):
-        settings = self.tenant.service_project_link.service.settings
-        settings.options['is_admin'] = False
-        settings.save()
-
-        self.client.force_authenticate(self.fixture.staff)
-        response = self.client.post(self.get_url())
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertFalse(mocked_task.called)
 
     def get_url(self):
         return factories.TenantFactory.get_url(self.tenant, 'pull')
@@ -142,16 +56,6 @@ class TenantDeleteTest(BaseTenantActionsTest):
         response = self.client.delete(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         mocked_task.assert_called_once_with(self.tenant, async=True, force=False)
-
-    def test_staff_can_not_delete_tenant_from_non_admin_service(self, mocked_task):
-        settings = self.tenant.service_project_link.service.settings
-        settings.options['is_admin'] = False
-        settings.save()
-
-        self.client.force_authenticate(self.fixture.staff)
-        response = self.client.delete(self.get_url())
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertFalse(mocked_task.called)
 
     def get_url(self):
         return factories.TenantFactory.get_url(self.tenant)
