411a31ed8bd37046630d67400d37c498fc617062
Pavel Marchuk <marchukpavelp@gmail.com>
2016-12-30 14:21:08 +0200

    Update dependency from nodeconductor
     - wal-288

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dev_requires = [
 
 install_requires = [
     'iptools>=0.6.1',
-    'nodeconductor>=0.111.0',
+    'nodeconductor>0.112.1',
     'python-ceilometerclient>=2.3.0',
     'python-cinderclient>=1.6.0',
     'python-glanceclient>=2.0.0',
