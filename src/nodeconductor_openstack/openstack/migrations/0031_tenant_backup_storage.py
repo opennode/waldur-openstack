7d3ffe4582a5b4032f9a59a3ea8edfded57a7a1f
tarasmatsyk <tamatsyk@gmail.com>
2017-02-01 15:09:57 +0200

    Clean up quota cleanup migration [WAL-433]

--- src/nodeconductor_openstack/openstack/migrations/0031_tenant_backup_storage.py
+++ src/nodeconductor_openstack/openstack/migrations/0031_tenant_backup_storage.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib.contenttypes.models import ContentType
 from django.db import migrations
-
-from nodeconductor.quotas import models as quotas_models
 
 from .. import models
 
 
 def cleanup_tenant_quotas(apps, schema_editor):
+    quota_names = models.Tenant.get_quotas_names()
     for obj in models.Tenant.objects.all():
-        quotas_names = models.Tenant.QUOTAS_NAMES + [f.name for f in models.Tenant.get_quotas_fields()]
-        obj.quotas.exclude(name__in=quotas_names).delete()
+        obj.quotas.exclude(name__in=quota_names).delete()
 
 
 class Migration(migrations.Migration):
