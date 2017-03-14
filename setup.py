ef1fc9f2ee1a4ab3f02905f600e89c7c218cb54a
tarasmatsyk <tamatsyk@gmail.com>
2017-03-14 15:26:28 +0200

    Update nodeconductor dependency version [WAL-548]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dev_requires = [
 
 install_requires = [
     'iptools>=0.6.1',
-    'nodeconductor>0.124.0',
+    'nodeconductor>0.126.1',
     'python-ceilometerclient>=2.3.0',
     'python-cinderclient>=1.6.0',
     'python-glanceclient>=2.0.0',
