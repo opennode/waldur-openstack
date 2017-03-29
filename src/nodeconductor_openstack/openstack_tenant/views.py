2a290ea71cd194fde4a147cbaf9c6363a517d2b7
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2017-03-29 21:48:40 +0300

    Validate volumes on instance backup creation
    - SENTRY-622

--- src/nodeconductor_openstack/openstack_tenant/views.py
+++ src/nodeconductor_openstack/openstack_tenant/views.py
@@ -420,6 +420,11 @@ class InstanceViewSet(six.with_metaclass(structure_views.ResourceViewMetaclass,
 
     @decorators.detail_route(methods=['post'])
     def backup(self, request, uuid=None):
+        instance = self.get_object()
+        if instance.volumes.count() != 2:
+            raise core_exceptions.IncorrectStateException(
+                'Instance can be backed up only with 1 bootable and 1 normal volumes.')
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         backup = serializer.save()
