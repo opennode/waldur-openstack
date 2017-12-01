988bd58869f5029ade1979390e335a865538bdb1
Jenkins <jenkins@opennodecloud.com>
2017-12-01 13:29:26 +0000

    Preparing new release: 0.38.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requires = [
 
 setup(
     name='waldur-openstack',
-    version='0.38.1',
+    version='0.38.2',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
