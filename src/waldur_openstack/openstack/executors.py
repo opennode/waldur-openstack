a75b9490dea8d8ceb679b33179a085cc06f389c1
Victor Mireyev <victor@opennodecloud.com>
2018-03-01 12:44:20 +0300

    Move PollRuntimeStateTask and PollBackendCheckTask from plugins to core [SENTRY-2460]

--- src/waldur_openstack/openstack/executors.py
+++ src/waldur_openstack/openstack/executors.py
@@ -196,21 +196,21 @@ class TenantDeleteExecutor(core_executors.DeleteExecutor):
             core_tasks.BackendMethodTask().si(
                 serialized_tenant, backend_method='delete_tenant_snapshots',
             ),
-            tasks.PollBackendCheckTask().si(
+            core_tasks.PollBackendCheckTask().si(
                 serialized_tenant,
                 backend_check_method='are_all_tenant_snapshots_deleted'
             ),
             core_tasks.BackendMethodTask().si(
                 serialized_tenant, backend_method='delete_tenant_instances',
             ),
-            tasks.PollBackendCheckTask().si(
+            core_tasks.PollBackendCheckTask().si(
                 serialized_tenant,
                 backend_check_method='are_all_tenant_instances_deleted'
             ),
             core_tasks.BackendMethodTask().si(
                 serialized_tenant, backend_method='delete_tenant_volumes',
             ),
-            tasks.PollBackendCheckTask().si(
+            core_tasks.PollBackendCheckTask().si(
                 serialized_tenant,
                 backend_check_method='are_all_tenant_volumes_deleted'
             ),
